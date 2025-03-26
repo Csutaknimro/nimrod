629a8c138cd292b87272e381937a191e66961560
Csutaknimro <nimrodcsutak@gmail.com>
2025-03-26 14:31:55 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -41,7 +41,18 @@ basic.showLeds(`
     . # . . #
     . # # # .
     `)
+basic.showNumber(10)
+basic.showNumber(9)
+basic.showNumber(8)
+basic.showNumber(7)
+basic.showNumber(6)
+basic.showNumber(5)
+basic.showNumber(4)
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
 })
