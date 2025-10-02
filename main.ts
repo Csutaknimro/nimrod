fc40c3f1e8411cb9529952cf999a8455c650ba26
Csutaknimro <nimrodcsutak@gmail.com>
2025-10-02 14:29:38 +0200

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -18,6 +18,7 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showString("HE")
+    basic.showString("NE CSINALD!!!!!")
 })
 input.onButtonPressed(Button.AB, function () {
 	
